fde3baa8fbcac50f7ba91ad2d2b838624b443864
LavanyaB1605 <100491624+LavanyaB1605@users.noreply.github.com>
2022-11-09 22:04:52 +0530

    Add files via upload

--- src/test/java/org/example/UserRegistration/UserRegistrationValidationTest.java
+++ src/test/java/org/example/UserRegistration/UserRegistrationValidationTest.java
@@ -15,10 +15,67 @@ public class UserRegistrationValidationTest {
         }
     }
 
-    @Test
+/*    @Test
     public void firstNameValidation_WithNumeric_ShouldFail() {
         try {
             userValidation.firstNameValidation("1234");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }*/
+/*    @Test
+    public void lastNameValidation_WithSplChar_ShouldFail() {
+        try {
+            userValidation.lastNameValidation("Lava!");
+        } catch (UserRegistrationException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    @Test
+    public void lastNameValidation_WithNumeric_ShouldFail() {
+        try {
+            userValidation.lastNameValidation("1234");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void EmailValidation_WithoutDomain_ShouldFail() {
+        try {
+            userValidation.emailValidation("abc.com");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void phoneNumberValidation_WhenShort_ShouldFail() {
+        try {
+            userValidation.phoneNumberValidation("98765");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void phoneNumberValidation_WithoutCountryCode_ShouldFail() {
+        try {
+            userValidation.phoneNumberValidation("9087654321");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void passwordValidation_WithoutUpperCase_ShouldFail() {
+        try {
+            userValidation.passwordValidation("lavnaya16@97");
+        }catch (UserRegistrationException e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void passwordValidation_WithoutNumeric_ShouldFail() {
+        try {
+            userValidation.passwordValidation("lavnaya007");
         }catch (UserRegistrationException e){
             e.printStackTrace();
         }
